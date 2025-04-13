ef3168351033eb9d9a4936d7fd19c3f15454d662
kap555 <113355222+kap555@users.noreply.github.com>
2025-04-13 15:30:55 +0300

    ыыы классная работа 13.04.2025
    ыавфыафыа

--- js-intro/js/2025-04-06.js
+++ js-intro/js/2025-04-06.js
@@ -1,34 +1,47 @@
-let a, b, sigma, result = NaN;
-
-a = Number(prompt("chislo A", "0"));
-b = Number(prompt("chislo B", "0"));
-
-sigma = prompt("Введи знак действия (+, -, *, /)", "+");
-
-switch (sigma) {
-    case "+":
-        result = a + b
-        break;
-    case "-":
-        result = a - b
-        break;
-    case "*":
-        result = a * b
-        break;
-    case "/":
-        if (b == 0) {
-            alert("Ты же помнишь, да?\nЧто на ноль делить нельзя :)\n\n:skull:")
-            result = NaN
-        } else {
-            result = a / b
-        }
-        break;
-    default:
-        alert("НЕИЗВЕСТНАЯ ОПЕРАЦИЯ НА ЯИЧКИ!!!")
+function calculate(sigma, a, b) {
+    let result = NaN;
+    switch (sigma) {
+        case "+":
+            result = a + b
+            break;
+        case "-":
+            result = a - b
+            break;
+        case "*":
+            result = a * b
+            break;
+        case "/":
+            if (b == 0) {
+                alert("Ты же помнишь, да?\nЧто на ноль делить нельзя :)\n\n:skull:")
+                result = NaN
+            } else {
+                result = a / b
+            }
+            break;
+        default:
+            alert("НЕИЗВЕСТНАЯ ОПЕРАЦИЯ НА ЯИЧКИ!!!")
+    }
+    return result
 }
 
-if (isNaN(result)) {
-    alert("Некорректный ввод! Немощь!")
-} else {
-    alert(`Ответ: ${result}`)
+function calculator() {
+    let a, b, sigma, result = NaN;
+    if (!confirm("Запустить КАЛКЬУККУЛУК КАЛЬКУЛЯТОР??")) return false;
+    a = Number(prompt("chislo A", "0"));
+    b = Number(prompt("chislo B", "0"));
+    
+    sigma = prompt("Введи знак действия (+, -, *, /)", "+");
+    
+    result = calculate(sigma, a, b)
+
+    if (isNaN(result)) {
+        alert("Некорректный ввод! Немощь!")
+    } else {
+        alert(`Ответ: ${result}`)
+    }
+    
+    return true;
 }
+let needRun = true
+
+while (needRun) needRun = calculator()
